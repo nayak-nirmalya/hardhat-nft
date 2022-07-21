dd4083636e110497e732ae35821ae3d739380d74
Nirmalya Nayak <n.nayak263@gmail.com>
2022-07-21 11:51:31 +0530

    first commit

---
+++ contracts/BasicNft.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+pragma solidity ^0.8.7;
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "DOG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter += 1;
+        return s_tokenCounter;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
