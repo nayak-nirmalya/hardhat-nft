9b2a6169b13737a8002bcd7a6ba748516546ed10
Nirmalya Nayak <n.nayak263@gmail.com>
2022-07-21 12:11:07 +0530

    added deply script

--- contracts/BasicNft.sol
+++ contracts/BasicNft.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity ^0.8.7;
 
 contract BasicNft is ERC721 {
+    string public constant TOKEN_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
     uint256 private s_tokenCounter;
 
     constructor() ERC721("Dogie", "DOG") {
@@ -15,6 +17,12 @@ contract BasicNft is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter += 1;
         return s_tokenCounter;
+    }
+
+    function tokenURI(
+        uint256 /* tokenId */
+    ) public pure override returns (string memory) {
+        return TOKEN_URI;
     }
 
     function getTokenCounter() public view returns (uint256) {
